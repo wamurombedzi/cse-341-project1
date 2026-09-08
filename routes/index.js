8d7389fbe96456a9d5d5cf150cc3c9b7d6b3148e
wamurombedzi <wadzamurombedzi>
2026-09-08 17:13:36 +0300

    Updating Get Endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  //#swagger.tags=['Hello World'] 
   res.send('Hello World'); 
 });
+
+router.use('/users', require('./users'));
 
 module.exports = router;
